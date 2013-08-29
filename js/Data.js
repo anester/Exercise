970c3ed3b3c7c3299c806ed24640e727d6a627b1
Adam Nester <bignester@gmail.com>
2013-08-29 14:47:39 -0500

    Initial Commit

---
+++ js/Data.js
@@ -0,0 +1,9 @@
+/// <reference file="libs/jquery-1.7.1.js" />
+/*
+This file will include web service hits right now it returns prefab stuff.
+*/
+var Svc = Svc || {};
+
+(function() {
+    
+})()
---
+++ js/Exercise.js
@@ -0,0 +1,4 @@
+/// <reference file="libs/jquery-1.7.1.js" />
+$(function() {
+    
+})
