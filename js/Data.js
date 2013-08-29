606b85f49664a72f8080c2780ef8217a9a887288
Adam Nester <bignester@gmail.com>
2013-08-29 15:56:45 -0500

    Incremental

--- js/Data.js
+++ js/Data.js
@@ -1,9 +1,30 @@
 /// <reference file="libs/jquery-1.7.1.js" />
 /*
-This file will include web service hits right now it returns prefab stuff.
+This file will include all DataModels used with javascript
 */
-var Svc = Svc || {};
+var TEMP = TEMP || {};
 
-(function() {
+TEMP.exercises = [
+    {
+        Name: '',
+        MajorMuscleGroup: '',
+        Description: '',
+        Difficulty: 0,
+        Images: [
+            {
+                Title: '',
+                Caption: '',
+                Order: 0,
+                Href: ''
+            }
+        ]
+    }
+];
+
+function ExerciseCollection() {
     
-})()
+}
+
+function ExerciseView() {
+    
+}
---
+++ js/Svc.js
@@ -0,0 +1,8 @@
+/*
+This file will include web service hits right now it returns prefab stuff.
+*/
+var Svc = Svc || {};
+
+(function() {
+    
+})()
